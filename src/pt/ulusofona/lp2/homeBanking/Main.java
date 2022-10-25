5e15e7ee6d07de00320cda6a51e52b6a2bb77c22
Rafael-Silva-a22107578 <rafael.lourenco.silva@gmail.com>
2022-10-25 09:53:46 +0100

    Adicionei a funcionalidade LALAAL

--- src/pt/ulusofona/lp2/homeBanking/Main.java
+++ src/pt/ulusofona/lp2/homeBanking/Main.java
@@ -4,7 +4,9 @@ import org.apache.groovy.contracts.ast.GContractsASTTransformation;
 
 public class Main {
 
+    // teste zero
     // teste
+    // Ola Mundo
     public static void main(String[] args){
         ContaBancaria conta = new ContaBancaria(100);
         conta.depositar(50);
